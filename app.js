03c24ecb4e1c9996eff7aa1f1e84156793d7e17d
Timothée Sterle <timothee-s@mailoo.org>
2019-08-13 15:05:03 +0200

    Boutons pause et stop

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let interval;
 let caTourne = false;
 let couleurBarre = "green";
 
+// Bouton "Lancer"
 id("lancer").addEventListener("click", function() {
 	if (caTourne === false) {
 		caTourne = true;
@@ -16,29 +17,69 @@ id("lancer").addEventListener("click", function() {
 	}
 });
 
+// Bouton "Pause"
+id("pause").addEventListener("click", function() {
+	caTourne = false;
+	clearInterval(interval);
+});
+
+// Bouton "Stop"
+id("stop").addEventListener("click", function() {
+	caTourne = false;
+	clearInterval(interval);
+	revenirDebut();
+});
+
 function retirerSeconde() {
+	secondes--;
 	let pourcentage = (100 * secondes) / 300;
-	
-	
+
+	// On commence à stresser quand il reste 60 secondes
 	if (secondes === 60) {
 		couleurBarre = "darkorange";
 	}
-	
+
+	// On stresse beaucoup quand il reste 15 secondes
 	if (secondes === 15) {
 		couleurBarre = "red";
 	}
-	
-	if (secondes === 0) {
-		secondes = 300;
-		couleurBarre = "green";
-	}
-	
-	id("prog").setAttribute("style", `width: ${pourcentage}%;background: ${couleurBarre};transition: 100ms`);
 
-	secondes--;
+	// On revient au début quand le chrono arrive à 0
+	if (secondes === 0) {
+		revenirDebut();
+	}
+
+	// On met à jour l'apparence de la barre de progression à chaque seconde
+	id("prog").setAttribute(
+		"style",
+		`width: ${pourcentage}%;background: ${couleurBarre};`
+	);
+
+	// On gère le formatage des minutes et des secondes
 	secondesFormate = secondes % 60;
 	minutesFormate = Math.floor(secondes / 60);
 	id("compteur").innerHTML = `${minutesFormate}:${("0" + secondesFormate)
 		.toString()
 		.slice(-2)}`;
 }
+
+function revenirDebut() {
+	secondes = 300;
+	couleurBarre = "green";
+	secondesFormate = secondes % 60;
+	minutesFormate = Math.floor(secondes / 60);
+	id("compteur").innerHTML = `${minutesFormate}:${("0" + secondesFormate)
+		.toString()
+		.slice(-2)}`;
+	id("prog").setAttribute("style", `width: 100%;background: green`);
+}
+
+
+// Quand on clique sur un joueur, sa carte se retourne
+document.body.onclick = function(e) {
+	e = e.target;
+
+	if (e.className && e.className.indexOf("joueurn") != -1) {
+		e.classList.add("retourne");
+	}
+};
